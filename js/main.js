98051d043f73d5c5e0fd584b38c6913658080235
Ella0707 <pakelvi77@gmail.com>
2025-09-02 10:11:57 +0300

    added fonts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+document.addEventListener('scroll', () => {
+    const boxes = document.querySelectorAll('.animate');
 
+    boxes.forEach(box => {
+        const boxPosition = box.getBoundingClientRect().top;
+
+        // Проверяем, виден ли элемент на экране
+        if (boxPosition < window.innerHeight && !box.classList.contains('visible')) {
+            box.classList.add('visible'); // Добавляем класс для анимации
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heroContent = document.querySelector('.first-screen');
+
+    // Появление анимации через 0.5 секунды после загрузки
+    setTimeout(() => {
+        heroContent.classList.add('visible');
+    }, 500);
+});
